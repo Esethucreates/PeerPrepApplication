475610f8f6b941a5cf32d89d89c508ee5b72e883
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-01-21 23:48:54 +0200

    Created to endpoints that handle the main page and the signup page

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinThymeleaf;
+import org.example.controllers.SignUpController;
 import org.example.controllers.UserController;
 import org.example.utilities.RenderEngine;
 
@@ -14,9 +15,11 @@ public class Main {
             javalinConfig.staticFiles.add("/public", Location.CLASSPATH);
             javalinConfig.fileRenderer(new JavalinThymeleaf(RenderEngine.thymeleafEngine()));
         }).start(1001);
-        app.get("/", ctx -> ctx.result("Hello World!"));
+        app.get("/", ctx -> ctx.render("/index.html"));
+        app.get("/signup", SignUpController::renderSignup);
 
-        app.get("/users", UserController::getAllUsers);
     }
+
+
 
 }
