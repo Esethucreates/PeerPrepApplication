3a56c930275e7e025a2f9e0527daf63a939dd81b
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-01-21 23:55:27 +0200

    Added a test case for CampusRepository

---
+++ src/test/java/org/example/repositories/CampusRepositoryTest.java
@@ -0,0 +1,17 @@
+package org.example.repositories;
+
+import org.example.models.CampusModel;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CampusRepositoryTest {
+
+    @Test
+    void returnCampuses() throws Exception {
+        var campusList = CampusRepository.returnCampuses();
+        assertNotNull(campusList.getFirst());
+    }
+}
