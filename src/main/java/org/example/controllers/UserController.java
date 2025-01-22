c8845384b5a762f9354ea1f0957f5aa71eab16e9
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-01-22 21:40:51 +0200

    Removed the createUser method

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -5,9 +5,14 @@ import org.example.models.UserModel;
 import org.example.repositories.UserRepository;
 
 import java.util.Map;
-record UserRequest (String fullName, String email,
-                    String password, String qualification, String userStatus, int campusID){}
-record ReturnUserWithoutPassword (String fullName, String email, String userStatus){}
+
+record UserRequest(String fullName, String email,
+                   String password, String qualification, String userStatus, int campusID) {
+}
+
+record ReturnUserWithoutPassword(String fullName, String email, String userStatus) {
+}
+
 public class UserController {
     //    Gets all users based on a specific param
     public static void getAllUsers(Context ctx) throws Exception {
@@ -21,21 +26,6 @@ public class UserController {
         //    Renders all users in an html
         ctx.render("/index.html", Map.of("tutorUsers", users));
 
-    }
-// TODO: Create a post function to create user
-
-
-    public static void createAUser(Context ctx) throws Exception{
-//        Validate the param
-        UserRequest body = ctx.bodyValidator(UserRequest.class)
-                .check(data -> data.password().length() > 6, "password must be greater than six characters")
-                .get();
-        UserModel user = UserRepository.createUser(body.fullName(), body.email(), body.password(), body.qualification(), body.userStatus(), body.campusID());
-
-        ctx.status(201);
-//        TODO: return user without password key
-        ReturnUserWithoutPassword parsedUser = new ReturnUserWithoutPassword(user.getFullName(), user.getEmail(), user.getUserStatus());
-        ctx.json(parsedUser);
     }
 
 }
