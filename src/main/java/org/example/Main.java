6ab5848ae41eef0352c71efa9bc80fb50b48e4ec
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-01-24 23:05:01 +0200

    Added new routes for new endpoints

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinThymeleaf;
-import org.example.controllers.HandleUserCreationForm;
-import org.example.controllers.SignUpController;
-import org.example.controllers.UserController;
+import org.example.controllers.*;
 import org.example.utilities.RenderEngine;
 
 
@@ -16,10 +14,21 @@ public class Main {
             javalinConfig.staticFiles.add("/public", Location.CLASSPATH);
             javalinConfig.fileRenderer(new JavalinThymeleaf(RenderEngine.thymeleafEngine()));
         }).start(1001);
+
+//        Main page endpoint
         app.get("/", ctx -> ctx.render("/index.html"));
+
+//        Sign Up page endpoint
         app.get("/signup", SignUpController::renderSignup);
-        app.post("/handleCreation", HandleUserCreationForm::handleUserCreation);
-        app.get("/dashboard", ctx -> ctx.render("/dashboard.html"));
+        app.post("/handleCreation", SignUpController::handleUserCreation);
+
+//        Login page endpoint
+        app.get("/login", ctx -> ctx.render("/login.html"));
+        app.get("/authenticate", LoginUserHandler::authenticateUser);
+
+//        Dashboard page endpoint
+        app.get("/dashboard", DashboardHandler::renderDashboard);
+
     }
 
 
