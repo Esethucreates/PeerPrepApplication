089f70ba1604966d385e79e91c06e9cddec2d19d
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-01-24 22:49:54 +0200

    Updated the env variables

--- src/main/java/org/example/utilities/DB.java
+++ src/main/java/org/example/utilities/DB.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 
 
 public class DB {
-    private static final Dotenv dotenv = Dotenv.configure().directory("src/.env").load();
+    private static final Dotenv dotenv = Dotenv.configure().directory("src/vars/.env").load();
 
     public static Connection connection() throws Exception{
         var dbURl = dotenv.get("DB_URL");
